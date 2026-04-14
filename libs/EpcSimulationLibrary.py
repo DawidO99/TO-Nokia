cec05592e755dacf5d49c46a0636dcf8fb96d90b
suwka <leczycki.krzysztoff@gmail.com>
2026-04-14 23:21:28 +0200

    zrobilem testy ja krzysztof leczycki

--- libs/EpcSimulationLibrary.py
+++ libs/EpcSimulationLibrary.py
@@ -1,1 +1,36 @@
-#TODO
+class EpcSimulationLibrary:
+    def __init__(self):
+        self.ues = {}
+
+    def reset_simulator(self):
+#tc_10 - reset symulatora (czyszczenie srodowiska)
+        self.ues.clear()
+
+    def attach_ue(self, ue_id):
+#tc_07 - proba podlaczenia juz aktywnego ue (test negatywny)
+        if str(ue_id) in self.ues:
+            raise Exception(f"error: ue {ue_id} already connected")
+        self.ues[str(ue_id)] = {"transfer": 0}
+
+    def start_dl_transfer(self, ue_id, speed, bearer_id=9):
+        val = int(''.join(filter(str.isdigit, speed)))
+#tc_09 - przekroczenie limitu transferu 100 mbps (test negatywny)
+        if "Mbps" in speed and val > 100:
+            raise Exception(f"error: speed {speed} exceeds limit")
+
+#tc_05 - rozpoczecie transferu downlink (sukces)
+        if str(ue_id) not in self.ues:
+            raise Exception("error: ue not connected")
+        self.ues[str(ue_id)]["transfer"] = val
+
+    def verify_ue_is_connected(self, ue_id):
+        if str(ue_id) not in self.ues: raise Exception("not connected")
+
+    def verify_ue_is_disconnected(self, ue_id):
+        if str(ue_id) in self.ues: raise Exception("still connected")
+
+    def verify_dl_transfer(self, ue_id, expected_speed):
+        if self.ues[str(ue_id)]["transfer"] == 0: raise Exception("no transfer")
+
+    def detach_ue(self, ue_id):
+        if str(ue_id) in self.ues: del self.ues[str(ue_id)]
--- resources/EPC_Keywords.robot
+++ resources/EPC_Keywords.robot
@@ -1,1 +1,30 @@
-#TODO
+*** Settings ***
+Library    ../libs/EpcSimulationLibrary.py
+
+*** Keywords ***
+Reset Simulator
+    Reset Simulator
+
+Attach UE
+    [Arguments]    ${id}
+    Attach UE    ${id}
+
+Detach UE
+    [Arguments]    ${id}
+    Detach UE    ${id}
+
+Start DL Transfer
+    [Arguments]    ${ue_id}    ${speed}    ${bearer_id}=9
+    Start DL Transfer    ${ue_id}    ${speed}    ${bearer_id}
+
+Verify DL Transfer
+    [Arguments]    ${ue_id}    ${expected_speed}
+    Verify DL Transfer    ${ue_id}    ${expected_speed}
+
+Verify UE Is Connected
+    [Arguments]    ${id}
+    Verify UE Is Connected    ${id}
+
+Verify UE Is Disconnected
+    [Arguments]    ${id}
+    Verify UE Is Disconnected    ${id}
